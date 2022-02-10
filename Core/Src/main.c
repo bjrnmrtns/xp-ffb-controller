cfaf51569e0332814afb93ef6c16c3f1bf0f68f0
Bjorn Martens <bjorn@expeditious.nl>
2022-02-10 18:13:19 +0100

    add hidget and hidOut and hidCmdCallback

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -132,9 +132,49 @@ void tud_cdc_tx_complete_cb(uint8_t itf){
 	HAL_UART_Transmit(&huart1, &buffer[0], strlen(buffer), 10);
 }
 
+uint16_t hidGet(uint8_t report_id, hid_report_type_t report_type,uint8_t* buffer, uint16_t reqlen){
+	char buf[] = "hidGet\r\n";
+	HAL_UART_Transmit(&huart1, &buf[0], strlen(buf), 10);
+	return 0;
+}
+
+void hidOut(uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize) {
+	char buf[] = "hidOut\r\n";
+	HAL_UART_Transmit(&huart1, &buf[0], strlen(buf), 10);
+}
+
+typedef enum HidCmdType  {write = 0, request = 1, info = 2, writeAddr = 3, requestAddr = 4,ACK = 10, notFound = 13, notification = 14, err = 15} HidCmdType_t;
+
+typedef struct
+{
+	uint8_t		reportId; // Report ID = 0xA1
+	HidCmdType_t type;				// 0x01. Type of report. 0 = write, 1 = request
+	uint16_t	clsid;				// 0x02 Class ID identifies the target class type
+	uint8_t 	instance;			// 0x03 Class instance number to target a specific instance (often 0). 0xff for broadcast to all instances
+	uint32_t	cmd;				// 0x04 Use this as an identifier for the command. upper 16 bits for class type
+	uint64_t	data;				// 0x05 Use this to transfer data or the primary value
+	uint64_t	addr;				// 0x06 Use this to transfer an optional address or second value (CAN for example)
+
+} __attribute__((packed)) HID_CMD_Data_t;
+
+
+void hidCmdCallback(HID_CMD_Data_t* data) {
+	char buf[] = "hidCmdCallback\r\n";
+	HAL_UART_Transmit(&huart1, &buf[0], strlen(buf), 10);
+}
+
 void tud_hid_set_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize){
 	char buf[] = "tud_hid_set_report_cb\r\n";
 	HAL_UART_Transmit(&huart1, &buf[0], strlen(buf), 10);
+	if(report_type == HID_REPORT_TYPE_INVALID && report_id == 0){
+		report_id = *buffer;
+	}
+
+	hidGet(report_id,report_type,buffer,bufsize);
+
+	if(report_id == HID_ID_HIDCMD) {
+		hidCmdCallback((HID_CMD_Data_t*)(buffer));
+	}
 }
 
 uint16_t tud_hid_get_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t report_type,uint8_t* buffer, uint16_t reqlen){
